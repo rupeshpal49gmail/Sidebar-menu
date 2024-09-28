f5df8ff29b5cb108c884ab7feb3230120da90dad
Rupesh Pal <rupeshpal49@gmail.com>
2024-09-29 00:38:42 +0530

    add last commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// let btn = document.getElementById("#btn");
+// let sidebar = document.getElementById(".sidebar");
+
+// btn.onclick = function () {
+//   sidebar.classList.toggle("active");
+//   console.log("Okay");
+// };
+
+alert("hello");
